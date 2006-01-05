6cfec5392f38ad62e9bfc7c1baef4f82ab978efc
tliebeck <tliebeck@be0c8feb-e106-0410-85bb-c64c24fcf685>
2006-01-05 09:11:18 +0000

    Border/bordertype properties added to Component impl.

--- src/app/java/nextapp/echo2/extras/app/TabPane.java
+++ src/app/java/nextapp/echo2/extras/app/TabPane.java
@@ -1,5 +1,6 @@
 package nextapp.echo2.extras.app;
 
+import nextapp.echo2.app.Border;
 import nextapp.echo2.app.Component;
 import nextapp.echo2.app.Extent;
 import nextapp.echo2.app.Pane;
@@ -13,15 +14,59 @@ implements Pane, PaneContainer {
     
     public static final String ACTIVE_TAB_CHANGED_PROPERTY = "activeTab";
     public static final String INPUT_ACTIVE_TAB = "activeTab";
-    
+
+    public static final String PROPERTY_BORDER = "border";
+    public static final String PROPERTY_BORDER_TYPE = "borderType";
     public static final String PROPERTY_TAB_HEIGHT = "tabHeight";
-    
     public static final String PROPERTY_TAB_POSITION = "tabPosition";
     public static final String PROPERTY_TAB_WIDTH = "tabWidth";
     
+    /**
+     * Constant for the <code>borderType</code> property indicating that no 
+     * border should be drawn around the content.
+     */
+    public static final int BORDER_TYPE_NONE = 0;
+
+    /**
+     * Constant for the <code>borderType</code> property indicating that a
+     * border should be drawn immediately adjacent to the tabs only.
+     * If the tabs are positioned at the top of the <code>TabPane</code> the
+     * border will only be drawn directly beneath the tabs with this setting.  
+     * If the tabs are positioned at the bottom of the <code>TabPane</code> the
+     * border will only be drawn directly above the tabs with this setting.
+     * This is the default rendering style.
+     */
+    public static final int BORDER_TYPE_ADJACENT_TO_TABS = 1;
+
+    /**
+     * Constant for the <code>borderType</code> property indicating that
+     * borders should be drawn above and below the content, but not at its 
+     * sides.
+     */
+    public static final int BORDER_TYPE_PARALLEL_TO_TABS = 2;
+    
+    /**
+     * Constant for the <code>borderType</code> property indicating that
+     * borders should be drawn on all sides of the content.
+     */
+    public static final int BORDER_TYPE_SURROUND = 3;
+    
+    /**
+     * Constant for the <code>tabPosition</code> property indicating that the
+     * tabs should be rendered beneath the content.
+     */
     public static final int TAB_POSITION_BOTTOM = 1;
+    
+    /**
+     * Constant for the <code>tabPosition</code> property indicating that the
+     * tabs should be rendered above the content.
+     * This is the default rendering style.
+     */
     public static final int TAB_POSITION_TOP = 0;
     
+    /**
+     * <code>renderId</code> of the active tab <code>Component</code>.
+     */
     private String activeTabRenderId;
     
     /**
@@ -53,6 +98,36 @@ implements Pane, PaneContainer {
             return getComponent(activeTabIndex);
         } else {
             return null;
+        }
+    }
+    
+    /**
+     * Returns the <code>Border</code> used to draw the <code>TabPane</code>.
+     * 
+     * @return the border
+     */
+    public Border getBorder() {
+        return (Border) getProperty(PROPERTY_BORDER);
+    }
+    
+    /**
+     * Returns the mode in which the border will be drawn around the 
+     * <code>TabPane</code>.
+     * 
+     * @return the border border type, one of the following values:
+     *         <ul>
+     *          <li><code>BORDER_TYPE_NONE</code></li>
+     *          <li><code>BORDER_TYPE_ADJACENT_TO_TABS</code> (the default)</li>
+     *          <li><code>BORDER_TYPE_PARALLEL_TO_TABS</code></li>
+     *          <li><code>BORDER_TYPE_SURROUND</code></li>
+     *         </ul>
+     */
+    public int getBorderType() {
+        Integer value = (Integer) getProperty(PROPERTY_BORDER_TYPE);
+        if (value == null) {
+            return BORDER_TYPE_ADJACENT_TO_TABS;
+        } else {
+            return value.intValue();
         }
     }
     
@@ -122,6 +197,31 @@ implements Pane, PaneContainer {
         }
     }
 
+    /**
+     * Sets the <code>Border</code> used to draw the <code>TabPane</code>.
+     * 
+     * @param newValue the new border
+     */
+    public void setBorder(Border newValue) {
+        setProperty(PROPERTY_BORDER, newValue);
+    }
+    
+    /**
+     * Sets the mode in which the border will be drawn around the 
+     * <code>TabPane</code>.
+     * 
+     * @param newValue the new border type one of the following values:
+     *        <ul>
+     *         <li><code>BORDER_TYPE_NONE</code></li>
+     *         <li><code>BORDER_TYPE_ADJACENT_TO_TABS</code> (the default)</li>
+     *         <li><code>BORDER_TYPE_PARALLEL_TO_TABS</code></li>
+     *         <li><code>BORDER_TYPE_SURROUND</code></li>
+     *        </ul>
+     */
+    public void setBorderType(int newValue) {
+        setProperty(PROPERTY_BORDER_TYPE, new Integer(newValue));
+    }
+    
     /**
      * Sets the height of an individual tab.
      * 
